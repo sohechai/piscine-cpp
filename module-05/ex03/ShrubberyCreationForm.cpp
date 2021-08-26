79a7ad52ad0caa5f498c55d0d31a5bfbf714ba55
Sofia Hechaïchi <sofiahechaichi@MacBook-Pro.local>
2021-08-26 20:41:36 +0200

    ex03 kc

--- module-05/ex03/ShrubberyCreationForm.cpp
+++ module-05/ex03/ShrubberyCreationForm.cpp
@@ -6,7 +6,7 @@
 /*   By: sohechai <sohechai@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 21:40:39 by sohechai          #+#    #+#             */
-/*   Updated: 2021/08/25 19:43:22 by sohechai         ###   ########lyon.fr   */
+/*   Updated: 2021/08/26 20:40:41 by sohechai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void				ShrubberyCreationForm::action(const std::string &target) const
 {
 	std::cout << "Creating " << target << "_shrubbery..." << std::endl;
 	std::ofstream	ofs(target + "_shrubbery");
-	ofs << "..O..O..O..O..O..O..O\n..|..|..|..|..|..|..|"; // TODO ajouter arbres ascii
+	ofs << "..O..O..O..O..O..O..O\n..|..|..|..|..|..|..|"; 
 	ofs.close();
 
 	return ;
